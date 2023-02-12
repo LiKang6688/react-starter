e65bdcfb42d4bc11e30c4575ab49963b86e53b5f
LiKang6688 <kkllpaul7766@gmail.com>
2023-02-12 15:23:22 +0100

    fix: format

--- src/app/store.ts
+++ src/app/store.ts
@@ -16,4 +16,3 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
